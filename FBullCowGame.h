c21cc6b16de7909084e86663966105b7042c6cfc
Dylan VanBrunt <dylan.vanbru@gmail.com>
2017-01-31 04:33:20 -0700

    Create FBullCowGame.h

---
+++ FBullCowGame.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <string>
+
+//make syntax unreal freindly
+using FString = std::string;
+using int32 = int;
+
+// all values intialised to zero
+struct FBullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
+
+enum class EGuessStatus
+{
+	Invalid,
+	OK,
+	Not_Isogram,
+	Wrong_Length,
+	Not_Lowercase
+};
+
+
+class FBullCowGame
+{
+public:
+	FBullCowGame(); // constructor
+	int32 GetMaxTries() const;
+	int32 GetCurrentTry() const;
+	int32 GetHiddenWordLength() const;
+	FString GetHiddenWord();
+	bool IsGameWon() const;
+
+	void SetHiddenWord(FString);
+	void SetGameIsWon();
+	void Reset(); // TODO make a more rich return value.
+
+	EGuessStatus CheckGuessValidity(FString) const;
+	FBullCowCount SubmitValidGuess(FString);
+
+	// ^^ Please try and ignore this and focus on the interface above ^^
+private:
+	// see constructor for initialisation
+	int32 MyCurrentTry;
+	FString MyHiddenWord;
+	bool bGameIsWon;
+
+	bool IsIsogram(FString) const;
+	bool IsLowercase(FString) const;
+};
